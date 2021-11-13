8f1dad346b09e83ca0517d7865eeec43440a9715
Roman <romachehdkdk@gmail.com>
2021-11-13 14:15:49 +0200

    Solved some bugs wirh redirect

--- web/modules/custom/custom_event/src/EventSubscriber/RedirectAnonymousSubscriber.php
+++ web/modules/custom/custom_event/src/EventSubscriber/RedirectAnonymousSubscriber.php
@@ -2,15 +2,50 @@
 
 namespace Drupal\custom_event\EventSubscriber;
 
-use Drupal\Component\EventDispatcher\Event;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Session\AccountInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * Redirect Anonymous to login page.
  */
 class RedirectAnonymousSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $account;
+
+  /**
+   * Messenger.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected MessengerInterface $messenger;
+
+  /**
+   * Route.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected RouteMatchInterface $route;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(MessengerInterface $messenger,
+                              AccountInterface $currentUser,
+                              RouteMatchInterface $route) {
+    $this->account = $currentUser;
+    $this->route = $route;
+    $this->messenger = $messenger;
+  }
 
   /**
    * {@inheritdoc}
@@ -23,13 +58,17 @@ class RedirectAnonymousSubscriber implements EventSubscriberInterface {
   /**
    * Check and redirect to login page.
    */
-  public function checkAuthStatus(Event $event) {
-    if (
-      \Drupal::currentUser()->isAnonymous() &&
-      \Drupal::routeMatch()->getRouteName() != 'user.login'
-    ) {
-      $response = new RedirectResponse('/user/login', 302);
-      $response->send();
+  public function checkAuthStatus(RequestEvent $event) {
+    $routes = [
+      'user.login',
+      'user.reset.login',
+      'user.reset',
+      'user.reset.form',
+      'user.pass',
+      'user.register',
+    ];
+    if ($this->account->isAnonymous() && !in_array($this->route->getRouteName(), $routes)) {
+      $event->setResponse(new RedirectResponse('/user/login', 302));
     }
   }
 
